3cfea669b0feb734b8ca77664011f03649abb0c5
TommasoVilla91 <tommasovilla@yahoo.it>
2024-12-03 15:25:48 +0100

    feat: CRUD operations (wip)

---
+++ data/food.js
@@ -0,0 +1,32 @@
+const recipes = [
+    {
+        titolo: "Ciambella",
+        contenuto: "Torta Savarin",
+        immagine: "ciambellone.jpeg",
+        tags: ["pasticceria", "uova", "latte", "zucchero"]        
+    },
+    {
+        titolo: "Cracker",
+        contenuto: "Cracker alla barbabietola",
+        immagine: "cracker_barbabietola.jpeg",
+        tags: ["croccante", "healthy", "biologico", "tostato"]
+    },
+    {
+        titolo: "Pane fritto",
+        contenuto: "Pane dolce fritto",
+        immagine: "pane_fritto_dolce.jpeg",
+        tags: ["croccante", "fritto", "farina", "zucchero"]
+    },
+    {
+        titolo: "Pasta",
+        contenuto: "Pasta alla barbabietola",
+        immagine: "pasta_barbabietola.jpeg",
+        tags: ["sugo", "vegetale", "biologico", "buona"]
+    },
+    {
+        titolo: "Torta",
+        contenuto: "Torta paesana",
+        immagine: "torta_paesana.jpeg",
+        tags: ["morbida", "pane", "cacao", "pinoli"]
+    },
+];
